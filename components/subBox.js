1d168846ba4e3e0fa416859cb3eb6e22f88a4ab6
Goldaxe <pierreleveque.pro@gmail.com>
2021-03-07 08:52:33 +0100

    teest

--- components/subBox.js
+++ components/subBox.js
@@ -3,10 +3,12 @@ import subBoxAnimation from '../lib/subBoxAnimatio'
 
 export default function SubBox() {
     const [boxActivate, setBoxActivate] = useState(false)
+    const [actualBox, setActualBox] = useState(false)
     const [inAnimation, setInAnimation] = useState(false)
     function activateBox (boxState, boxActivate, inAnimation) {
         if(!inAnimation){
             setInAnimation(true)
+            setActualBox(boxState)
             subBoxAnimation(boxState, ()=>{setBoxActivate(x => !x), setInAnimation(false)},boxActivate)
         }
     }
@@ -15,18 +17,18 @@ export default function SubBox() {
         <div className="subBox--container">
             <div className="subBox">
                 <div className="subBox__box amethyst" onClick={() => activateBox(0, boxActivate, inAnimation)}>
-                    {boxActivate ?
+                    {boxActivate && !inAnimation && actualBox === 0 ?
                         <div className="subBox__inBox">
-                            <h3>BASIC</h3>
-                            <p>L'abonnement de base telle qu'il à été pensé</p>
+                            <p className="subBox__inBox__title">BASIC</p>
+                            <p  className="subBox__inBox__subTitle">L'abonnement de base telle qu'il à été pensé</p>
                             <h4>Avantages :</h4>
                             <ul>
-                                <li>- Accès à tous les salons du Discord !</li>   
-                                <li>- Certification automatique (@Certifié) !</li> 
-                                <li>- Des salons exclusifs pour les subs !</li> 
-                                <li>- Un message de remerciement de la part du fondateur sur Patreon !</li> 
+                                <li>- Accès à <span>tous les salons</span> du Discord !</li>   
+                                <li>- <span>Certification automatique</span> (@<span>Certifié</span>) !</li> 
+                                <li>- Des <span>salons exclusifs</span> pour les subs !</li> 
+                                <li>- Un <span>message de remerciement</span> de la part du fondateur sur Patreon !</li> 
                             </ul>
-                            <div class="subbBox__inBox__price">
+                            <div className="subBox__inBox__price">
                                 <p>1€ symbolique par mois.</p>
                                 <p>1€</p>
                             </div>
@@ -37,22 +39,81 @@ export default function SubBox() {
                             <p className="subBox__box__price">1€</p>
                         </>
                     }
-
                 </div>
 
                 <div className="subBox__box lava" onClick={() => activateBox(1, boxActivate, inAnimation)}>
-                    <p className="subBox__box__title">SUPER</p>
-                    <p className="subBox__box__price">5€</p>
+                    {boxActivate && !inAnimation  && actualBox === 1 ?
+                        <div className="subBox__inBox">
+                            <p className="subBox__inBox__title">BASIC</p>
+                            <p  className="subBox__inBox__subTitle">L'abonnement de base telle qu'il à été pensé</p>
+                            <h4>Avantages :</h4>
+                            <ul>
+                                <li>- Accès à <span>tous les salons</span> du Discord !</li>   
+                                <li>- <span>Certification automatique</span> (@<span>Certifié</span>) !</li> 
+                                <li>- Des <span>salons exclusifs</span> pour les subs !</li> 
+                                <li>- Un <span>message de remerciement</span> de la part du fondateur sur Patreon !</li> 
+                            </ul>
+                            <div className="subBox__inBox__price">
+                                <p>1€ symbolique par mois.</p>
+                                <p>1€</p>
+                            </div>
+                        </div>
+                    :
+                        <>
+                            <p className="subBox__box__title">BASIC</p>
+                            <p className="subBox__box__price">1€</p>
+                        </>
+                    }
                 </div>
 
                 <div className="subBox__box emerald" onClick={() => activateBox(2, boxActivate, inAnimation)}>
-                    <p className="subBox__box__title">PREMIUM</p>
-                    <p className="subBox__box__price">10€</p>
+                    {boxActivate && actualBox === 2 ?
+                        <div className="subBox__inBox">
+                            <p className="subBox__inBox__title">BASIC</p>
+                            <p  className="subBox__inBox__subTitle">L'abonnement de base telle qu'il à été pensé</p>
+                            <h4>Avantages :</h4>
+                            <ul>
+                                <li>- Accès à <span>tous les salons</span> du Discord !</li>   
+                                <li>- <span>Certification automatique</span> (@<span>Certifié</span>) !</li> 
+                                <li>- Des <span>salons exclusifs</span> pour les subs !</li> 
+                                <li>- Un <span>message de remerciement</span> de la part du fondateur sur Patreon !</li> 
+                            </ul>
+                            <div className="subBox__inBox__price">
+                                <p>1€ symbolique par mois.</p>
+                                <p>1€</p>
+                            </div>
+                        </div>
+                    :
+                        <>
+                            <p className="subBox__box__title">BASIC</p>
+                            <p className="subBox__box__price">1€</p>
+                        </>
+                    }
                 </div>
 
                 <div className="subBox__box sky" onClick={() => activateBox(3, boxActivate, inAnimation)}>
-                    <p className="subBox__box__title">ULTIMATE</p>
-                    <p className="subBox__box__price">30€</p>
+                {boxActivate && actualBox === 3 ?
+                    <div className="subBox__inBox">
+                        <p className="subBox__inBox__title">BASIC</p>
+                        <p  className="subBox__inBox__subTitle">L'abonnement de base telle qu'il à été pensé</p>
+                        <h4>Avantages :</h4>
+                        <ul>
+                            <li>- Accès à <span>tous les salons</span> du Discord !</li>   
+                            <li>- <span>Certification automatique</span> (@<span>Certifié</span>) !</li> 
+                            <li>- Des <span>salons exclusifs</span> pour les subs !</li> 
+                            <li>- Un <span>message de remerciement</span> de la part du fondateur sur Patreon !</li> 
+                        </ul>
+                        <div className="subBox__inBox__price">
+                            <p>1€ symbolique par mois.</p>
+                            <p>1€</p>
+                        </div>
+                    </div>
+                    :
+                        <>
+                            <p className="subBox__box__title">BASIC</p>
+                            <p className="subBox__box__price">1€</p>
+                        </>
+                    }
                 </div>
             </div>
         </div>
