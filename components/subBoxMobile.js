54584712fb7e3f646229effcf591b203a3017529
Goldaxe <pierreleveque.pro@gmail.com>
2021-03-07 10:46:51 +0100

    azeaz

--- components/subBoxMobile.js
+++ components/subBoxMobile.js
@@ -29,20 +29,22 @@ function SubBoxBox({i, color}) {
 
     return (
         <div className={`subBoxMobile__box ${color}`} onClick={() => activateBox(boxActivate, inAnimation)}>
-            {boxActivate ?
+            {boxActivate && !inAnimation ?
                 <div className="subBox__inBox">
-                    <p className="subBox__inBox__title">BASIC</p>
-                    <p  className="subBox__inBox__subTitle">L'abonnement de base telle qu'il à été pensé</p>
-                    <h4>Avantages :</h4>
-                    <ul>
-                        <li>- Accès à <span>tous les salons</span> du Discord !</li>   
-                        <li>- <span>Certification automatique</span> (@<span>Certifié</span>) !</li> 
-                        <li>- Des <span>salons exclusifs</span> pour les subs !</li> 
-                        <li>- Un <span>message de remerciement</span> de la part du fondateur sur Patreon !</li> 
-                    </ul>
+                    <div>
+                        <p className="subBox__inBox__title">BASIC</p>
+                        <p  className="subBox__inBox__subTitle">L'abonnement de base telle qu'il à été pensé</p>
+                        <h4>Avantages :</h4>
+                        <ul>
+                            <li>- Accès à <span>tous les salons</span> du Discord !</li>   
+                            <li>- <span>Certification automatique</span> (@<span>Certifié</span>) !</li> 
+                            <li>- Des <span>salons exclusifs</span> pour les subs !</li> 
+                            <li>- Un <span>message de remerciement</span> de la part du fondateur sur Patreon !</li> 
+                        </ul>
+                    </div>
                     <div className="subBox__inBox__price">
-                        <p>1€ symbolique par mois.</p>
-                        <p>1€</p>
+                        <p className="subBox__inBox__price__text">1€ symbolique par mois.</p>
+                        <p className="subBox__inBox__price__value">1€</p>
                     </div>
                 </div>
             :
